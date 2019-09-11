9dd1d8bc44f4bfba2a12a1b96c5b974fa2e71323
Mike <mjwalshe@gmail.com>
2019-09-11 12:25:58 +0100

    Adjust indentation in bookmark_spec

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -7,26 +7,26 @@ describe Bookmark do
       Bookmark.add('http://www.destroyallsoftware.com')
       Bookmark.add('http://www.google.com')
 
-    bookmarks = Bookmark.all
+      bookmarks = Bookmark.all
 
-    expect(bookmarks).to include('http://www.makersacademy.com')
-    expect(bookmarks).to include('http://www.google.com')
-    expect(bookmarks).to include('http://www.destroyallsoftware.com')
+      expect(bookmarks).to include('http://www.makersacademy.com')
+      expect(bookmarks).to include('http://www.google.com')
+      expect(bookmarks).to include('http://www.destroyallsoftware.com')
     end
   end
 
   describe '.add' do
     it 'adds a bookmark to the database' do
-    Bookmark.add('http://www.makersacademy.com')
-    Bookmark.add('http://www.destroyallsoftware.com')
-    Bookmark.add('http://www.google.com')
-    Bookmark.add('http://www.XXX.com')
-    bookmarks = Bookmark.all
+      Bookmark.add('http://www.makersacademy.com')
+      Bookmark.add('http://www.destroyallsoftware.com')
+      Bookmark.add('http://www.google.com')
+      Bookmark.add('http://www.wikipedia.org/wiki/Bookmark')
+      bookmarks = Bookmark.all
 
-    expect(bookmarks).to include('http://www.makersacademy.com')
-    expect(bookmarks).to include('http://www.google.com')
-    expect(bookmarks).to include('http://www.destroyallsoftware.com')
-    expect(bookmarks).to include('http://www.XXX.com')
+      expect(bookmarks).to include('http://www.makersacademy.com')
+      expect(bookmarks).to include('http://www.google.com')
+      expect(bookmarks).to include('http://www.destroyallsoftware.com')
+      expect(bookmarks).to include('http://www.wikipedia.org/wiki/Bookmark')
     end
   end
 end
